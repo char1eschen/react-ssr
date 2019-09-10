d477fe7cb5b5bd609fd52a229314ae7046f9e9c4
Charles Chen <martian2005@gmail.com>
2019-09-10 23:19:45 +1000

    redux test

---
+++ src/containers/Home/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Header from "../../components/Header";
+import { connect } from "react-redux";
+import { getHomeList } from "./store/actions";
+
+class Home extends Component {
+  getList() {
+    const { list } = this.props;
+    return list.map(item => <div key={item.created_date}>{item.title}</div>);
+  }
+
+  render() {
+    console.log(this.props.list);
+    return (
+      <div>
+        <Header />
+        {this.getList()}
+        <button
+          onClick={() => {
+            alert("click");
+          }}
+        >
+          Click
+        </button>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.props.getHomeList();
+  }
+}
+
+const mapStateToProps = state => ({
+  list: state.home.newsList
+});
+
+const mapDispatchToProps = dispatch => ({
+  getHomeList() {
+    dispatch(getHomeList());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
